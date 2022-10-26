c03a8cc5e53a11cb228d7285d63f6bdb8f398146
abhijithmca <116720154+abhijithmca@users.noreply.github.com>
2022-10-26 16:07:42 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,5 @@
+# to find the area and perimeter of rectangle
+l=int(input("Enter the length:"))
+b=int(input("Enter the breadth:"))
+print("area of rectangle:",l*b)
+print("perimeter of rectangle:",2*(l+b))
